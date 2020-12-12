e531f2b8eb1640ec664a328c017ed3c120080ef4
tudormuresan07 <tudorvladmuresan@gmail.com>
2020-12-12 20:33:13 +0200

    RightURLs

--- medicaltests_project/analysis/urls.py
+++ medicaltests_project/analysis/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 app_name='analysis'
 urlpatterns=[
-    path('',views.index,name='index'),
+    path('',views.home,name='home'),
+    path('add',views.add,name='add'),
+    path('chat',views.chat,name='chat'),
+    path('statistics',views.statistics,name='statistics'),
+    path('myprofile',views.myprofile,name='myprofile'),
+    path('notifications',views.notifications,name='notifications'),
+    path('showall',views.show_all,name='show_all'),
     ]
--- medicaltests_project/analysis/views.py
+++ medicaltests_project/analysis/views.py
@@ -5,12 +5,42 @@ from django.urls import reverse
 from django.views import generic
 from .models import *
 
-def index(request):
+def home(request):
     latest_analysis_list=Result.objects.all()
-    template=loader.get_template('navbar.html')
+    template=loader.get_template('analysis/home_page.html')
     context={
         'results':
         latest_analysis_list
         }
 
+    return HttpResponse(template.render(context,request))
+
+def add(request):
+    template=loader.get_template('analysis/add_entity.html')
+    context={}
+    return HttpResponse(template.render(context,request))
+
+def chat(request):
+    template=loader.get_template('analysis/chat.html')
+    context={}
+    return HttpResponse(template.render(context,request))
+
+def statistics(request):
+    template=loader.get_template('analysis/statistics.html')
+    context={}
+    return HttpResponse(template.render(context,request))
+
+def myprofile(request):
+    template=loader.get_template('analysis/my_profile.html')
+    context={}
+    return HttpResponse(template.render(context,request))
+
+def notifications(request):
+    template=loader.get_template('analysis/notifications.html')
+    context={}
+    return HttpResponse(template.render(context,request))
+
+def show_all(request):
+    template=loader.get_template('analysis/show_all.html')
+    context={}
     return HttpResponse(template.render(context,request))
