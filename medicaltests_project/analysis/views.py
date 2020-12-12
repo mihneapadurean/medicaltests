5ae150f274c0f98d3b753a84fc937c5a264502e8
tudormuresan07 <tudorvladmuresan@gmail.com>
2020-12-12 16:45:31 +0200

    BootDone

--- medicaltests_project/analysis/views.py
+++ medicaltests_project/analysis/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from .models import *
 
-# Create your views here.
+def index(request):
+    latest_analysis_list=Result.objects.all()
+    context={
+        'latest_analysis_list':
+        latest_analysis_list
+        }
+
+    stringRes=""
+    for a in latest_analysis_list:
+        stringRes+=str(a)+"\n"
+
+
+    return HttpResponse(stringRes)
